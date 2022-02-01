6fe61304f88decc8305076b5465b226401b18496
romai <r.abdeselam.2021@alumnos.urjc.es>
2022-02-01 16:19:53 +0100

    second change

--- HELLO/hello.py
+++ HELLO/hello.py
@@ -1,2 +1,8 @@
 print("This is just a test for us to know how the thing works")
 print("Again!!")
+# commit lo que hace es guardarlo en el ordenador y push hace que se suba a github
+# para execute tenemos varias formas --> python.exe .\Hello\hello.py
+# las otras dos formas es coger y darle a la ventana del folder y darle a run o sino meterse en RUN (cerca de terminal) y le das a la flecha verde
+#tener cuidado con la última opción porque tenemos que mirar la ventana que esta a la izq. si tiene el mismo nombre
+#un breakpoint (círulo rojo), utilizar el debugger, la consola, y leer el código (run it ) step by step
+#code 0 --> que se ha ejecutado bien
