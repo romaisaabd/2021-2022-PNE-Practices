d14a209efe9e08238c3156afb218270fb96b6c1b
romai <r.abdeselam.2021@alumnos.urjc.es>
2022-03-29 15:20:30 +0200

    -

---
+++ Session-10/happy_server.py
@@ -0,0 +1,29 @@
+import socket
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "localhost"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+# -- Waits for a client to connect
+print("Waiting for Clients to connect")
+ls.accept()
+
+print("A client has connected to the server!")
+
+# -- Close the socket
+ls.close()
+
+
+
+#nestat - an |FINDSTR (PUERTO EN EL QUE ESTÁ JUGANDO)
